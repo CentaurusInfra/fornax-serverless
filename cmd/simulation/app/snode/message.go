3715b41cece410de2ec83c56e6c364cc88f5e24e
davidzhu22 <90352709+davidzhu22@users.noreply.github.com>
2022-08-01 09:33:44 -0700

    simulation initial version, need cleanup
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-9-84.us-west-2.compute.internal>

---
+++ cmd/simulation/app/snode/message.go
@@ -0,0 +1,66 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package snode
+
+import (
+	"centaurusinfra.io/fornax-serverless/pkg/fornaxcore/grpc"
+	"centaurusinfra.io/fornax-serverless/pkg/nodeagent/pod"
+)
+
+func BuildFornaxGrpcNodeState(node *FornaxNode) *grpc.FornaxCoreMessage {
+	podStates := []*grpc.PodState{}
+	for _, v := range node.Pods {
+		s := pod.BuildFornaxcoreGrpcPodState(node.Revision, v)
+		podStates = append(podStates, s.GetPodState())
+	}
+	ns := grpc.NodeState{
+		NodeRevision:  &node.Revision,
+		Node:          node.V1Node,
+		PodStates:     podStates,
+		SessionStates: []*grpc.SessionState{},
+	}
+
+	messageType := grpc.MessageType_NODE_STATE
+	return &grpc.FornaxCoreMessage{
+		MessageType: &messageType,
+		MessageBody: &grpc.FornaxCoreMessage_NodeState{
+			NodeState: &ns,
+		},
+	}
+}
+
+func BuildFornaxGrpcNodeReady(node *FornaxNode) *grpc.FornaxCoreMessage {
+	podStates := []*grpc.PodState{}
+	for _, v := range node.Pods {
+		s := pod.BuildFornaxcoreGrpcPodState(node.Revision, v)
+		podStates = append(podStates, s.GetPodState())
+	}
+	ns := grpc.NodeReady{
+		NodeRevision:  &node.Revision,
+		Node:          node.V1Node,
+		PodStates:     podStates,
+		SessionStates: []*grpc.SessionState{},
+	}
+
+	messageType := grpc.MessageType_NODE_READY
+	return &grpc.FornaxCoreMessage{
+		MessageType: &messageType,
+		MessageBody: &grpc.FornaxCoreMessage_NodeReady{
+			NodeReady: &ns,
+		},
+	}
+}
